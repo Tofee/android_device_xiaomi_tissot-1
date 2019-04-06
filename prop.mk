bb3b11b08d31f8729226c91610c7e5b6e42bc8ca
mountaser <mountaser1998@gmail.com>
2019-04-06 12:22:06 +0530

    tissot: Update props • Enable HAL3 • Enable EIS • Add GCam to aux list

--- prop.mk
+++ prop.mk
@@ -7,7 +7,10 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+persist.camera.HAL3.enabled=1 \
+persist.camera.eis.enable=1 \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
 persist.camera.dual.camera=0
 
 # Dalvik
